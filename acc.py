1d8b6c71c4d6a68a3dc20a237926058904af6837
mery200 <61519067+mery200@users.noreply.github.com>
2021-05-23 22:15:25 +0100

    initial launch

--- acc.py
+++ acc.py
@@ -1241,7 +1241,7 @@ def render_page_content(pathname):
         [
             html.H1("404: Not found", className="text-danger"),
             html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
+            html.P(f"The pathname {pathname} was not recognised!..."),
         ]
     )
 
